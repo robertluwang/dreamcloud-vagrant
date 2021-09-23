9cf7b0578ff655861d60dbd54aeac35da002692e
Robert Wang <robert.lu.wang@gmail.com>
2021-09-22 23:10:21 -0400

    Update ubuntu-k8sbox.sh

--- scripts/ubuntu-k8sbox.sh
+++ scripts/ubuntu-k8sbox.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#  ubuntu 18.04 k8s 1.20.8-00 box provision script for packer  
+#  ubuntu 20.04 k8s 1.21 box provision script for packer  
 #  Robert Wang
-#  Jun 21, 2021
+#  Sept 22, 2021
 
 # update/upgrade system 
-apt-get update -y 
-apt-get upgrade -y 
+apt-get update && apt-get upgrade -y 
 
 # install docker
 apt-get install -y docker.io
@@ -15,12 +14,11 @@ systemctl start docker
 usermod -aG docker vagrant
 
 # turn off swap
-swapoff -a
-sed -i '/swap/d' /etc/fstab
+sudo swapoff -a
+sudo sed -i '/swap/d' /etc/fstab
 
 # install k8s 
-sh -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list'
-curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-apt-get update -y 
-apt-get install -y -q kubelet=1.20.8-00 kubeadm=1.20.8-00 kubectl=1.20.8-00
+echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' >> /etc/apt/sources.list.d/kubernetes.list
+curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+apt-get update && apt-get install -y -q kubelet=1.21.4-00 kubeadm=1.21.4-00 kubectl=1.21.4-00 
 apt-mark hold kubelet kubeadm kubectl
